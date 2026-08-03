f88d0f74891665d68b61d1ad23b543233d176f39
RJ <rybertjen@gmail.com>
2026-08-03 09:44:42 -0700

    Wire CLI binary entrypoint

--- crates/invokrum-cli/src/main.rs
+++ crates/invokrum-cli/src/main.rs
@@ -1,5 +1,8 @@
 #![forbid(unsafe_code)]
 
 fn main() {
-    println!("invokrum {}", env!("CARGO_PKG_VERSION"));
+    let mut stdout = std::io::stdout().lock();
+    let mut stderr = std::io::stderr().lock();
+    let code = invokrum_cli::run(std::env::args_os().skip(1), &mut stdout, &mut stderr);
+    std::process::exit(code);
 }
